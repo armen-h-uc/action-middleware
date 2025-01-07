78718713a2f3b0109fde4118dad3fa5770c48cf7
Armen Hovhannisyan <armen.hov@ucraft.com>
2025-01-07 18:12:28 +0400

    fix php doc

--- src/ActionMiddlewareResponse/ValidationResponse.php
+++ src/ActionMiddlewareResponse/ValidationResponse.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Uc\ActionMiddleware\ActionMiddlewareResponse;
 
+use Illuminate\Support\Facades\Validator as ValidatorFacade;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Validator;
 
 class ValidationResponse extends ActionMiddlewareResponse
 {
@@ -23,7 +25,23 @@ class ValidationResponse extends ActionMiddlewareResponse
     public function handle(): void
     {
         if (!$this->isSuccess) {
-            throw ValidationException::withMessages($this->messages);
+            throw new ValidationException($this->getValidator());
         }
+    }
+
+    /**
+     * @return \Illuminate\Validation\Validator
+     */
+    protected function getValidator(): Validator
+    {
+        $rules = [];
+
+        foreach ($this->messages as $field => $message) {
+            $rules[$field] = function ($attribute, $value, $fail) use ($message): void {
+                $fail($message);
+            };
+        }
+
+        return ValidatorFacade::make($this->inputData, $rules);
     }
 }
